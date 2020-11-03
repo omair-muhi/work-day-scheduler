eaddd86017e03491f228a58dea3dc61a3f3d95e1
Omair Muhi <omairmuhi@icloud.com>
2020-11-02 22:09:12 -0500

    Subject: Add localDB functionality Problem: NA Solution: NA Note: NA

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,27 @@
+function saveBtnHandler(event) {
+    // 1. Get the text and id of the event
+    // console.log(event.target);
+    // console.log($(event.target).parent().prev().val());
+    var timeBlockEventText = $(event.target).parent().prev().val();
+    var timeBlockId = parseInt($(event.target).parent().prev().attr("id") - 9);
+    // 2. Read localDB
+    var calendarEvtsLclDb = JSON.parse(localStorage.getItem("calendarEvents"));
+    if (calendarEvtsLclDb !== null) {
+        calendarEvents = calendarEvtsLclDb;
+    }
+    // 3. Modify contents
+    calendarEvents.timeBlocksArray[timeBlockId].push(timeBlockEventText);
+    // 4. Write back to localDb
+    localStorage.setItem("calendarEvents", JSON.stringify(calendarEvents));
+    // console.log("Text:" + timeBlockEventText);
+    // console.log("Button #" + timeBlockId);
+    // localStorage.setItem("calendarEvents", JSON.stringify(calendarEvents));
+}
+
 function refreshTimeBlockColors() {
     // Colour code based on current time
-    var currentHour = parseInt(moment().format('H'));
+    // var currentHour = parseInt(moment().format('H'));
+    var currentHour = 10;
     var calendarBegin = parseInt($("input:first").attr("id"));
     var calendarEnd = parseInt($("input:last").attr("id"));
     if (currentHour < calendarBegin) {
@@ -20,7 +41,7 @@ function refreshTimeBlockColors() {
         if (i < numTimeBlocks - 1) {
             currentTimeBlockEnd = parseInt($(inputTagArray[i + 1]).attr("id"));
         } else {
-            currentTimeBlockEnd = 18; // hard-coded
+            currentTimeBlockEnd = 18; // last business-hour hard-coded
         }
         if ((currentHour >= currentTimeBlockBegin) && (currentHour < currentTimeBlockEnd)) {
             // Set background colour to red
@@ -34,12 +55,33 @@ function refreshTimeBlockColors() {
     }
 
 }
+var calendarEvents = {
+    timeBlocksArray: [
+        [], // 0900
+        [], // 1000
+        [], // 1100
+        [], // 1200
+        [], // 1300
+        [], // 1400
+        [], // 1500
+        [], // 1600
+        [] // 1700
+    ]
+}
+
+function loadEventsFromLocalDb() {
+    var calendarEvtsLclDb = JSON.parse(localStorage.getItem("calendarEvents"));
+    if (calendarEvtsLclDb !== null)
+        calendarEvents = calendarEvtsLclDb;
+}
 
 function initApplication() {
     // Display today's date at top
     var currentMoment = moment().format("dddd, MMMM Do");
     $("#currentDay").text(currentMoment);
+    $("button").click(saveBtnHandler);
     refreshTimeBlockColors();
+    // loadEventsFromLocalDb();
 }
 
 $(document).ready(initApplication);
